4c38882353141970bda09a027a81fe8b73ba02b2
tropical-express <gnome2009123@gmail.com>
2026-05-28 23:32:48 -0500

    Optimize code to the best of its abilities"

---
+++ electron/main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
